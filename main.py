c6009ad0c97529d75260a4e6a8a6d8a2d57951a8
= <lemur123123@gmail.com>
2025-01-20 18:48:15 +0100

    change main.py

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 import tkinter as tk
+from contextlib import nullcontext
 
-root= tk.Tk()
-root.title('Personal Planner')
-root.geometry('800x500')
+class PersonalPlannerApp:
+    def __init__(self,root):
+        self.root = root
+        self.root.title('Personal Planner')
+        self.root.geometry('800x500')
 
-label = tk.Label(root, text="test test test", font=('Arial',16))
-label.pack(pady=20)
+        header=tk.Label(root, text='Planner Personalny', font=('Arial',16))
+        header.pack(pady=10)
+
+        task_entry=tk.Entry(root, width=50, font=('System',14))
+        task_entry.pack(pady=10)
+        add_task_button=tk.Button(root, text='Dodaj zadanie', command=nullcontext, font=('Arial',12))
+        add_task_button.pack()
+
+
+
+
+root = tk.Tk()
+app = PersonalPlannerApp(root)
 root.mainloop()
